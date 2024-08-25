3dfb1f2badfdfd64a60f75b8ac1ab9eebb24c7ff
HesandaLiyanage <nhesanda@gmail.com>
2024-08-25 10:13:30 +0530

    Added battlefield.c and get the grid input

---
+++ battleships.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int R,C,N,x,y;
+    scanf("%d %d %d" , &R , &C , &N);
+    int grid[R][N];
+
+    for (int i = 0 ; i < R ; i++){
+        for (int j = 0 ; j < C ; j++){
+            scanf("%d" , grid[i][j]);
+        }
+    }
+    for (int i = 0; i < N ; i++){
+        
+    }
+
+
+
+}
